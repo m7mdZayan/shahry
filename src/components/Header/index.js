50b3612374d071b5633efbcebf595b1744f67877
Moahmed Zayan <mohamed.zayan@valify.me>
2022-10-13 22:26:33 +0200

    add header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,14 @@
 const Header = () => {
-    return ( <header>
-        <div className="header__content">
-            <h1>what our users say about shahry</h1>
-            <button>add a review</button>
-        </div>
-    </header> );
-}
- 
+  return (
+    <header>
+      <div className="header__content">
+        <h1 className="header__content--text">
+          what our users say about shahry ?
+        </h1>
+        <button className="btn btn-secondary">add a review</button>
+      </div>
+    </header>
+  );
+};
+
 export default Header;
