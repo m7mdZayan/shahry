2dca13543e659dd520bd118362e8f05f4a57483f
Moahmed Zayan <mohamed.zayan@valify.me>
2022-10-13 22:36:25 +0200

    add website wrapper

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ const accountImg = "https://randomuser.me/api/portraits/men/3.jpg";
 
 function App() {
   return (
-    <>
+    <div className="wrapper">
       <Navbar userName="Mohamed Zayan" accountImg={accountImg} />
       <Header />
-    </>
+    </div>
   );
 }
 
